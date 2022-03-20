da37500ee97b6f3d36c2606734170fb1a91824f6
JSparksssss <37327269+JSparksssss@users.noreply.github.com>
2022-03-20 23:15:09 +0000

    Update

--- app/forms.py
+++ app/forms.py
@@ -1,11 +1,8 @@
 from django import forms
 from django.contrib.auth.models import User
 from app.models import UserProfile
-
-
-class UserForm(forms.ModelForm):
-    email = forms.EmailField()
-
+   
+class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
-        fields = ('name', 'email', 'password', )
+        fields = ('name', 'password','email','money')
--- app/views.py
+++ app/views.py
@@ -1,39 +1,67 @@
+from multiprocessing import context
+from ssl import _create_default_https_context
 from django.contrib import auth
 from app.models import UserProfile
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.urls import reverse
 from django.views.generic import View, ListView
 import tkinter.messagebox
 from tkinter import *
 
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render, redirect
-from app.forms import UserForm
 from django.views.generic import View
 from django.http import HttpResponse
+from app.models import Goods,UserProfile,Likes,User
 
-
+#Display Groceries
 def index(request):
-    return render(request, 'pages/index.html')
+    context_dict = {}
+    try:
+        u_name = request.COOKIE.get('username')
+        user =  UserProfile.objects.get(name=u_name) 
+        likes = Likes.objects.filter(like_from=user)
+        context_dict['like_goods'] = []
+        for like in likes:
+            good = Goods.objects.filter(good=like.like_to)
+            context_dict['like_goods'].append(good)
+        goods = Goods.objects.all()
+        context_dict['goods'] = goods
+    except Exception as e:
+        goods = Goods.objects.all()
+        context_dict['goods'] = goods
+        context_dict['like_goods'] = None
+    return render(request, 'pages/index.html',context = context_dict)
 
+#Update like table
+def like_good(request,good_name):
+    try:
+        u_name = request.COOKIE.get('username')
+        user = UserProfile.objects.get(name=u_name) 
+        good = Goods.objects.filter(name = good_name)
+        #How to update sql in sqlite
+
+        return redirect(reverse('app:index'))
+    except Exception as e:
+        print("add like fail.")
 
 def register(request):
     # if request.method == 'GET':
     #     return render(request, 'pages/registration.html')
     if request.method == 'POST':
         name = request.POST.get('name')
-        # print(name)
         email = request.POST.get('email')
-        # print(email)
         password = request.POST.get('password')
-        # print(password)
 
         try:
-            user1 = UserProfile.objects.create(name=name, password=password, email=email)
+
+            user = UserProfile.objects.create(name=name, password=password, email=email)
+            user.save()
+
             return render(request, 'pages/login.html')
         except Exception as e:
             print(str(e))
     return render(request, 'pages/registration.html')
-
 
 def login(request):
     # if request.method == 'GET':
@@ -42,12 +70,13 @@ def login(request):
         # print('1')
         username = request.POST.get('username')
         password = request.POST.get('password')
+        
         # print(username)
         # print(password)
         try:
             user = UserProfile.objects.get(name=username)
             if user.password == password:
-                return render(request, 'pages/registration.html')
+                return redirect(reverse('app:index'))
             else:
                 HttpResponse("Incorrect password")
         except Exception as e:
