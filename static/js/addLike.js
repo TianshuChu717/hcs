b53fd5f5bb274e596306c02592e870cecbf7b2d7
Xuandong Guo <2427543166@qq.com>
2022-03-21 13:35:36 +0000

    update addlike

--- static/js/addLike.js
+++ static/js/addLike.js
@@ -15,9 +15,7 @@ function add(name){
             }
             else if(result==1){
                 alert("succesfully added");
-                // 做一下网页刷新
-                // window.location.href='#';
-
+                window.location.href = '#';
             }
             else{
                 alert("error happened");
