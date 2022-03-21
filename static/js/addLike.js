4dd29c105933a50ad163745c591319b876355cb1
JSparksssss <37327269+JSparksssss@users.noreply.github.com>
2022-03-21 10:43:54 +0000

    Update

--- static/js/addLike.js
+++ static/js/addLike.js
@@ -3,7 +3,7 @@ function add(name){
     // const good = $('#name').val();
 
     const good =name
-
+    console.log("Adding grocery:",name)
     $.ajax({
         type: 'POST',
         url: '/app/add_like/',
